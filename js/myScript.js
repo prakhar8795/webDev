88f9ecdcd7af0c2c3051452fa9e7a7b3b5efdbb4
Prakhar <prakhar8795@gmail.com>
2016-12-23 23:28:17 +0530

    Fixed Mobile Responsiveness

--- js/myScript.js
+++ js/myScript.js
@@ -42,4 +42,38 @@ $(document).ready(function() {
 
 		}
 	});
+
+	$(".btn-group-vertical>.btn").click(function() {
+		console.log("I'm") ;
+		if($(this).hasClass("btn-default")) {
+			console.log("Here") ;
+			console.log($(".btn-group-vertical").find(".btn-primary").text()) ;
+			
+			var currentActive = $(".btn-group-vertical").find(".btn-primary").text() ;
+			if(currentActive=="Post Graduate") {
+				$("#postgraduate").addClass("hidden") ;
+			}
+			else if(currentActive=="Under Graduate") {
+				$("#undergraduate").addClass("hidden") ;
+			}
+			else {
+				$("#phd").addClass("hidden") ;
+			}
+			
+			var clickedElement = $(this).text() ;
+			if(clickedElement=="Post Graduate") {
+				$("#postgraduate").removeClass("hidden") ;
+			}
+			else if(clickedElement=="Under Graduate") {
+				$("#undergraduate").removeClass("hidden") ;
+			}
+			else {
+				$("#phd").removeClass("hidden") ;
+			}
+			
+			$(".btn-group-vertical").find(".btn-primary").removeClass("btn-primary").addClass("btn-default") ;
+			$(this).removeClass("btn-default").addClass("btn-primary") ;
+
+		}
+	});
 });
